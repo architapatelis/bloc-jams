1b57724534828a0b336e86a6a18ea09bc3c5fba8
Archita Patel <architapatelis@gmail.com>
2017-02-26 16:06:09 -0800

    DOM Scripting: Album View fix typo and missing code

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,7 @@ var albumMarconi = {
     ]
 };
 
+
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
         '<tr class="album-view-song-item">'
@@ -42,6 +43,8 @@ var createSongRow = function (songNumber, songName, songLength) {
     return template;
 };
 
+
+
 var setCurrentAlbum = function(album) {
     //#1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -49,7 +52,8 @@ var setCurrentAlbum = function(album) {
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
+
+
     //#2
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -57,11 +61,16 @@ var setCurrentAlbum = function(album) {
     albumImage.setAttribute('src', album.albumArtUrl);
     
     //#3
+    albumSongList.innerHTML = '';
+    
+    //#4
     for(var i=0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
 
+
 window.onload = function () {
     setCurrentAlbum(albumPicasso);
-}
+    
+};
