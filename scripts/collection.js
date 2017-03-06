ee5be6dbb5251ebae8f378453017ca1ea7a30f19
Archita Patel <architapatelis@gmail.com>
2017-03-06 10:15:55 -0800

    jQuery: Collection and Album Views

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,28 +1,36 @@
-var collectionItemTemplate =    
-    '<div class="collection-album-container column fourth">'
-+'      <img src="assets/images/album_covers/01.png" alt="album cover">'
-+'      <div class="collection-album-info caption">'
-+'          <p>'
-+'              <a class="album-name" href="album.html">The Colors</a>'
-+'              <br/>'
-+'              <a href="album.html">Pablo Picasso</a>'
-+'              <br/>'
-+'              X songs'
-+'              <br/>'
-+'          </p>'
-+'      </div>'
-+'</div>'
-;
+// when naming action-oriented functions, it's a convention to start the function name with a verb.
+var buildcollectionItemTemplate = function () { 
+    var template = 
+     '<div class="collection-album-container column fourth">'
+    +'  <img src="assets/images/album_covers/01.png" alt="album cover">'
+    +'  <div class="collection-album-info caption">'
+    +'      <p>'
+    +'          <a class="album-name" href="album.html">The Colors</a>'
+    +'          <br/>'
+    +'          <a href="album.html">Pablo Picasso</a>'
+    +'          <br/>'
+    +'          X songs'
+    +'          <br/>'
+    +'      </p>'
+    +'  </div>'
+    +'  </div>'
+    ;
+    // wrap template in a jQuery object, in case we want to use jQuery methods on it in the future. 
+    return $(template);
+};
 
-window.onload = function () {
+
+$(window).load(function() {
     // The  collection of album is contained within the "album-covers" class element (<section>)
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $('.album-covers');
     
-    // Clear the contents of the HTML just in case something else has been dynamically inserted
-    collectionContainer.innerHTML = '';
+    // Clear the contents(children) of collectionContainer section just in case something else has been dynamically inserted
+    $collectionContainer.empty();
     
     // Insert N number of albums into the collectionContainer (<section>)
     for(var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildcollectionItemTemplate();
+        // With each loop, we append the template content to the collection container.
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
